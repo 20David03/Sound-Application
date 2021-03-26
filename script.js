428965b8dcd550e9cf9a29d861d3b980d4240c81
20David03 <davoadamyan64@gmail.com>
2021-03-26 22:24:54 +0400

    commit

--- script.js
+++ script.js
@@ -10,22 +10,23 @@ window.addEventListener("load", ()=>{
             sounds[index].play()
             pad.onmouseout = function() {
                     pad.style.opacity=1
-
+                   pad.style.boxShadow = "0px 0px 20px 6px #e5432b"
             };
         }
         else{
             sounds[index].pause()
                      pad.onmouseout = function() {
                     pad.style.opacity=0.2
-
+                    pad.style.boxShadow = "0px 0px 0px 0px"
             };
         }
 
         sounds[index].onended=function(){
             pad.style.opacity=0.2
+            pad.style.boxShadow = "0px 0px 0px 0px"
             pad.onmouseout = function() {
                 pad.style.opacity=0.2
-
+                
         };
         }
 
